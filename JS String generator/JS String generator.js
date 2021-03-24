2b270f1ee9092d016e4ed4cc988daee03b28ff86
DD2476 <DragosDragos2476@gmail.com>
2021-03-24 14:50:56 +0200

    Update JS String generator.js

--- JS String generator/JS String generator.js
+++ JS String generator/JS String generator.js
@@ -1,4 +1,14 @@
 //MADE BY DD2476
+
+///README///
+
+//set the length of the string at line 6
+//write the characters it can use at line 22 (example: "abcd1234", they must have no spaces between them!)
+
+//IMPORTANT: To run it, install node.js and save the file as -something-.js . Then, open cmd, navigate to the directory of -something-.js and type "node -something-.js". That will use node.js to run your file.
+
+//it will generate strings with the characters you gave it
+
 let x = 0;
 
 function script() {
@@ -39,10 +49,3 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
     
 }
-
-//set the length of the string at line 6
-//write the characters it can use at line 22 (example: "abcd1234", they must have no spaces between them!)
-
-//IMPORTANT: To run it, install node.js and save the file as -something-.js . Then, open cmd, navigate to the directory of -something-.js and type "node -something-.js". That will use node.js to run your file.
-
-//it will generate strings with the characters you gave it
